dd4264877cc8700dda0a7aebf63abf91482be868
GRU <evbolohov@yandex.ru>
2021-10-21 23:07:46 +0300

    1

--- Занятие1/Домашнее_задание/task1/main.py
+++ Занятие1/Домашнее_задание/task1/main.py
@@ -3,12 +3,11 @@ if __name__ == "__main__":
     speed = int(input("Введите скорость передачи данных байт/сек: "))
     time = int(input("Время загрузки данных мин.: "))
     coast = int(input("Стоимость трафика данных за Гб: "))
-  # Переводим время в секунды
+    # Переводим время в секунды
     time_sec = time * 60
-   # Переводим скорость передачи данных в Гб/сек
+    # Переводим скорость передачи данных в Гб/сек
     speed_gb_sec = speed / (1024 * 1024 * 1024)
-   # Размер файла в ГБ
-    # filesize = time_sec * speed_gb_sec
- print("Размер файла: ", int(time_sec * speed_gb_sec)
-
-    pass
+    # Размер файла в ГБ
+    filesize = time_sec * speed_gb_sec
+    print("Стоимость: ", ((filesize - 1) / coast))
+    print("Размер файла (Гб): ", (time_sec * speed_gb_sec))
--- Занятие1/Лабораторные задания/task8/main.py
+++ Занятие1/Лабораторные задания/task8/main.py
@@ -1,3 +1,11 @@
 if __name__ == "__main__":
     list_ = [8, 9, -5, -3, 1, -10, 8, -10, -5, 0, 5, -4, 0, 10, -8, 1, 6, -6, 6, -9]
+    unique_numbers = set(list_)
+
+    sum_ = sum(unique_numbers)
+    len_ = len(unique_numbers)
+
+    print(sum_)
+    print(len_)
+    print(sum_ / len_)
     # TODO найти суммуб количество и среднее уникальных чисел
