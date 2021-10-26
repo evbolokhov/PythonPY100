e0aa3eed9601b4e2f1364e7e52274bd0b0a3178d
GRU <evbolohov@yandex.ru>
2021-10-26 23:27:13 +0300

    1

--- Занятие1/Домашнее_задание/task2/main.py
+++ Занятие1/Домашнее_задание/task2/main.py
@@ -1,3 +1,5 @@
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    time_one_task = int(input("Среднее время на одно задание (мин.): "))
+    # all_time = round(((time_one_task * 10) / 60), 1)
+    print("Кол-во часов на выполнение д/з: ", round(((time_one_task * 10) / 60), 1))
+
